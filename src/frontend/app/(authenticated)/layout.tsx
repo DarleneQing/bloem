b2ba1423c69d9be7e13537bd9e628272776e1470
DarleneQing <ophlia.cn@gmail.com>
2025-10-23 22:57:02 +0200

    add package and components for Vercel

--- src/frontend/app/(authenticated)/layout.tsx
+++ src/frontend/app/(authenticated)/layout.tsx
@@ -1,6 +1,8 @@
 import { ProtectedRoute } from "@/components/auth/protected-route";
 import { BottomNav } from "@/components/layout/bottom-nav";
 import { Header } from "@/components/layout/header";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function AuthenticatedLayout({
   children,
@@ -12,7 +14,9 @@ export default function AuthenticatedLayout({
       <div className="min-h-screen pb-16 md:pb-0">
         <Header />
         <main className="min-h-[calc(100vh-4rem)]">
-          {children}
+            {children}
+            <Analytics/>
+            <SpeedInsights/>
         </main>
         <BottomNav />
       </div>
